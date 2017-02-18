576f5ff70d95f4e51ba042b0f3b2a311e43b8a75
youzaiyouzai666 <1620030379@qq.com>
2017-02-18 17:14:15 +0800

    base

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 /**
  * Created by Administrator on 2017/2/17.
  */
-module.exports = {
-    port: 8080,
-    session: {
-        secret: 'cblog',
-        key: 'cblog',
-        maxAge: 2592000000
-    },
-    mongodb: 'mongodb://localhost:27017/cblog'
-};
+const express = require('express');
+const app     = express();
+
+app.get('/', (req, res) => {
+    res.send('blog');
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    const err  = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error   = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
