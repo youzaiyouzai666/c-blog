f64a172fe16cd85fee3c20544b671ae0a91b4037
youzaiyouzai666 <1620030379@qq.com>
2017-03-17 18:34:52 +0800

    promise test

--- public/js/home.js
+++ public/js/home.js
@@ -12,6 +12,43 @@ require.config({
 
 require(["jquery", "common/util", "common/config","common/common"],
     function ($, util, config,common) {
+        function getPromise(){
+           return  Promise.resolve(util.ajax(
+                {
+                    url : config.api.user.login,
+                    data: {"name":"test"},
+                    type: 'POST'
+                }
+            ));
+        }
+
+        getPromise().then(function(data){
+            console.log(data);
+            return getPromise();
+        }).catch(function(e){
+            console.log(e);
+        }).then(
+            function(data){
+                console.log(data);
+            }
+        );
+      /*  function* gen(){
+            var result = yield getPromise();
+            var re2 = yield getPromise();
+        }
+
+        let g = gen();
+        let result = g.next();
+        result.value.then(function(data){
+            console.log(data);
+        }).then(function(data){
+            console.log(data);
+            return g.next(data);
+        }).then(function(data){
+            console.log(data);
+        })*/
+
+
 
 });
 
